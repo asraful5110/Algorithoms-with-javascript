1964167d4a01f0029104fe7f505fb950990342f0
Asraful islam <asrafulislam181082@gmail.com>
2023-11-04 09:55:47 +0600

    change arrary value

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let arr = [4,3,88,5,3,3,6,,4,2,,56,8,4,3,9];
+let arr = [4,3,88,5,3,3,6,1,4,2,7,56,8,4,3,9];
 
 
 function bubbleSort(arr){
-    let len = arr.length-1;
+    let len = arr.length;
     for(let i = 0; i < len; i++){
         for(let j = 0; j <= len; j++){
             if(arr[i] < arr[j]){
