26d8832ca19d9c9e13328a735bcf0d35f3525cea
Asraful islam <asrafulislam181082@gmail.com>
2023-11-04 10:50:54 +0600

    optimize bubble sort

--- script.js
+++ script.js
@@ -18,4 +18,21 @@ function bubbleSort(arr){
 
 let res = bubbleSort(arr);
 
+
+function bubbleSort2(arr){
+let len = arr.length-1;
+for(let i = 0; i < len; i++){
+    for(let j = i+1; j < len; j++){
+      if(arr[i] > arr[j]){
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+}
+return arr;
+}
+
+res = bubbleSort2(arr);
+
 console.log(res)
