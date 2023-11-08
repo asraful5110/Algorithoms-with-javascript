53182c7e54a023e90752fb8107d4ffa6373cbed9
Asraful islam <asrafulislam181082@gmail.com>
2023-11-08 23:36:36 +0600

    count number

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('This is main branch')
+let arr = [2,2,4,4,5];
+let obj = {
+    name : "Asraful islam",
+    country :  "Bangladesh"
+}
+
+// Type string
+
+
+function displayNumber(){
+    let num = 0;
+
+    setInterval(()=>{
+        if(num === 10) return;
+        num++
+        console.log(num)
+    },1000)
+  
+
+   
+}
+
+
+displayNumber()
