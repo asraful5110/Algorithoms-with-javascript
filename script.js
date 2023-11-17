b21358afd7fab88de716c4c67126685f3bcc4ee5
Asraful islam <asrafulislam181082@gmail.com>
2023-11-17 14:09:06 +0600

    new file added

--- script.js
+++ script.js
@@ -10,15 +10,44 @@ let obj = {
 function displayNumber(){
     let num = 0;
 
-    setInterval(()=>{
+    let res = setInterval(()=>{
         if(num === 10) return;
         num++
         console.log(num)
     },1000)
   
 
+   if(num === 2){
+    clearInterval(res)
+   }
    
 }
 
 
-displayNumber()
+//displayNumber()
+
+let type = document.querySelector('.type')
+let names = []
+function typeString(res){
+
+        let str = "Asraful";
+        let i = 0;
+       
+        setInterval(()=>{
+           if(i === str.length){
+              names = [];
+              i = 0;
+             }
+           names.push(str[i]);
+           type.innerHTML = names.join('')
+         i++;
+        },100)
+    
+    }
+
+
+
+typeString(true)
+
+
+console.log("Hello bangladesh")
